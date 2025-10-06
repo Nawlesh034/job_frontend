c99e3877e782ddc29f3bf876deb49bb18e154bcb
Nawlesh Nand <info@admiregroup.org>
2025-10-06 09:11:47 +0530

    test-1

--- axios.js
+++ axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const api = axios.create({
-  baseURL:import.meta.env.VITE_API_URL|| "http://localhost:5000",
+  baseURL:import.meta.env.VITE_API_URL|| "https://job-backend-1-9k6b.onrender.com",
   withCredentials: true, // important if using httpOnly cookies
 });
 
